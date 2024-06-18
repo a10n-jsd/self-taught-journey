506ff1b67700a6da3019d0bb51cbc87e87c7593f
Ayid Sudirman <ayidsudirman@protonmail.com>
2024-06-19 03:35:00 +0800

    Practice Scenario 3 - joining a team

---
+++ git-and-github/simon-bae/scenarios/joining-a-team/code.js
@@ -0,0 +1,5 @@
+console.log("code");
+
+// Just joining a team and their code is already on github. How to start contributing?
+// 1. Clone the project
+// 2. Start contributing code as normal: make some changes, commit it and push it to remote
