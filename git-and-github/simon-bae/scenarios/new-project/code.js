626bb012dacbd26e17e6a290214a1ff185755415
Ayid Sudirman <ayidsudirman@protonmail.com>
2024-06-18 13:33:20 +0800

    Practice Scenario 2 - new project

---
+++ git-and-github/simon-bae/scenarios/new-project/code.js
@@ -0,0 +1,9 @@
+console.log("code");
+
+// Two ways to integrate git and github:
+// A. The existing-project way (see existing-project folder)
+// B. The remote-first way:
+// B1. Make new github repository first
+// B2. Clone the remote to local machine
+// B3. commit the change
+// B4. push your change to remote
