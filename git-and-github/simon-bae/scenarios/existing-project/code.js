8f49b110447ab053b9a6b86ec061d6d0d5a1d297
Ayid Sudirman <ayidsudirman@protonmail.com>
2024-06-18 13:02:54 +0800

    Practice Scenario 1 - existing project

---
+++ git-and-github/simon-bae/scenarios/existing-project/code.js
@@ -0,0 +1,8 @@
+console.log("code");
+
+// Existing project with several steps
+// 1. initializing git
+// 2. commit with message
+// 3. make a repository on github
+// 4. Do not forget to rename branch from Master to Main
+// 5. Push to remote
