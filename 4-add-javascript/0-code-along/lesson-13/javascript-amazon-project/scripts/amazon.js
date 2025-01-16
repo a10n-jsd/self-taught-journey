dda3ec45b84edf6a5bfc203cbbd2b4842e251430
Ayid Sudirman <ayidsudirman@protonmail.com>
2025-01-17 06:42:48 +0800

    lesson 13: use products.js file

--- 4-add-javascript/0-code-along/lesson-13/javascript-amazon-project/scripts/amazon.js
+++ 4-add-javascript/0-code-along/lesson-13/javascript-amazon-project/scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    image: 'https://i.ibb.co.com/0CCmcj4/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090 
-  },
-  {
-    image: 'https://i.ibb.co.com/mNVWRGC/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4.0,
-      count: 127
-    },
-    priceCents: 2095 
-  },
-  {
-    image: 'https://i.ibb.co.com/V3msDdj/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799 
-  },
-];
-
 let productsHTML = '';
 
 products.forEach((product) => {
