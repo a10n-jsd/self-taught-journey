fbc1133c7d82379b5c26d317f6dcb674aebfccc9
Ayid Sudirman <ayidsudirman@protonmail.com>
2025-02-08 15:05:40 +0800

    remove duplicate code with hooks: beforeEach()

--- 4-add-javascript/0-code-along/lesson-16/javascript-amazon-project/tests-jasmine/scripts/checkout/orderSummaryTest.js
+++ 4-add-javascript/0-code-along/lesson-16/javascript-amazon-project/tests-jasmine/scripts/checkout/orderSummaryTest.js
@@ -6,18 +6,19 @@ describe('test suite: renderOrderSummary', () => {
   // 1. How the page looks
   // 2. How the page behaves
 
-  it('displays the cart', () => {
+  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
     // Since we don't have element with class 'js-order-summary' in the test
     // we make one and put it a div to be it's container 
     document.querySelector('.js-test-container').innerHTML = `
+      <div class="js-checkout-header"></div>
       <div class="js-order-summary"></div>
+      <div class="js-payment-summary"></div>
     `;
 
-    // renderOrderSummary take data from the cart
-    // we don't want to make a mess within localStorage
-    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([
         {
@@ -35,9 +36,10 @@ describe('test suite: renderOrderSummary', () => {
     });
 
     loadFromStorage();
-
     renderOrderSummary();
+  })
 
+  it('displays the cart', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
     ).toEqual(2);
@@ -54,35 +56,6 @@ describe('test suite: renderOrderSummary', () => {
   });
 
   it('removes a product', () => {
-    spyOn(localStorage, 'setItem');
-    document.querySelector('.js-test-container').innerHTML = `
-      <div class="js-checkout-header"></div>
-      <div class="js-order-summary"></div>
-      <div class="js-payment-summary"></div>
-    `;
-
-    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([
-        {
-          productId: productId1,
-          quantity: 2,
-          deliveryOptionId: '1'
-          
-        },
-        {
-          productId: productId2,
-          quantity: 1,
-          deliveryOptionId: '2'
-        }
-      ]);
-    });
-
-    loadFromStorage();
-
-    renderOrderSummary();
-
     document.querySelector(`.js-delete-link-${productId1}`).click();
 
     // Check that after delete product-1, the cart only contain an item
