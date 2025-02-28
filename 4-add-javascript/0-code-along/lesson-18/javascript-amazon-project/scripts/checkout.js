76ec5a3526d13bf1d812e7e319cef875780d4d51
Ayid Sudirman <ayidsudirman@protonmail.com>
2025-03-01 07:03:02 +0800

    lesson 18: use Promise.all instead

--- 4-add-javascript/0-code-along/lesson-18/javascript-amazon-project/scripts/checkout.js
+++ 4-add-javascript/0-code-along/lesson-18/javascript-amazon-project/scripts/checkout.js
@@ -16,7 +16,28 @@ import { loadCart } from "../data/products.js";
 // 3. Make it interactive (Controller)
 // Note: MVC is design pattern in software development
 
+Promise.all([
+  new Promise((resolve) => {
+    // resolve() same with done() from jasmine for controlling when to go to next step
+    loadProducts(() => {
+      resolve('a product value');
+    })
+  
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('a cart value');
+    })
+  })
 
+]).then((values) => {
+  console.log(values); // values[0], values[1]
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 new Promise((resolve) => {
   // resolve() same with done() from jasmine for controlling when to go to next step
   loadProducts(() => {
@@ -38,7 +59,7 @@ new Promise((resolve) => {
   renderOrderSummary();
   renderPaymentSummary();
 })
-
+*/
 
 // callback with anonymous function
 // the problem with callback: a lot of nesting
